0d7a4fc33d99b83aa241c38fabc58c4f950774c1
Gustavo Gelinski <gus_gelinski@hotmail.com>
2023-05-15 09:58:00 -0300

    Comentários adicionados em app.py. Bootstrap adicionado em home.html

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html")
 
+
+"""
+Para testar qualquer html que estiver fazendo, é só tirar o comentário desse trecho de código. # noqa: E501
+No lugar de "teste.html", colocar nome do arquivo .html que estiver localizado na pasta templates.
+Na url, colocar http://127.0.0.1:5000/teste
+
+@app.route("/teste")
+def hello_world():
+    return render_template("teste.html")
+"""
 
 if __name__ == '__main__':
     app.run(debug=True)
