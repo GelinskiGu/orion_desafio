a9cde0803d10997337fe4d8174dace0363551d70
Gustavo Gelinski <gus_gelinski@hotmail.com>
2023-05-15 11:17:41 -0300

    Nova configuração para db.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:8rPTT7k#gT@localhost/orion_desafio.db'  # noqa: E501
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 
