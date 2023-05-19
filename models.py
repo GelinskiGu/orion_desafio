46b5af75012f840fce1ca6819b0e9c7db9b9e3ab
Gustavo Gelinski <gus_gelinski@hotmail.com>
2023-05-19 19:09:33 -0300

    Recipes atualizada.

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import LargeBinary
+from sqlalchemy import LargeBinary, Integer, String, ForeignKey, DateTime, func
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import UploadSet, IMAGES
@@ -11,10 +11,10 @@ photos = UploadSet('photos', IMAGES)
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
+    id = db.Column(Integer, primary_key=True)
+    username = db.Column(String(20), nullable=False, unique=True)
     password = db.Column(LargeBinary, nullable=False)
-    name = db.Column(db.String(255), nullable=False)
+    name = db.Column(String(255), nullable=False)
 
     def __init__(self, username, password, name):
         self.username = username
@@ -25,8 +25,8 @@ class User(UserMixin, db.Model):
 class Category(db.Model):
     __tablename__ = 'categories'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), nullable=False, unique=True)
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(64), nullable=False, unique=True)
 
     def __init__(self, name):
         self.name = name
@@ -35,17 +35,17 @@ class Category(db.Model):
 class Recipe(db.Model):
     __tablename__ = 'recipes'
 
-    id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.Integer, db.ForeignKey('users.id'))
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    id = db.Column(Integer, primary_key=True)
+    author = db.Column(Integer, ForeignKey('users.id'))
+    category_id = db.Column(Integer, ForeignKey('categories.id'))
     category = db.relationship("Category")
-    title = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.String, nullable=True)
-    ingredients = db.Column(db.String, nullable=True)
-    preparation_steps = db.Column(db.String, nullable=True)
-    created_at = db.Column(db.DateTime, nullable=False)
-    image_filename = db.Column(db.String, nullable=True)
-    image_path = db.Column(db.String, nullable=True)
+    title = db.Column(String(255), nullable=False)
+    description = db.Column(String, nullable=True)
+    ingredients = db.Column(String, nullable=True)
+    preparation_steps = db.Column(String, nullable=True)
+    created_at = db.Column(DateTime, nullable=False, default=func.now())
+    image_filename = db.Column(String, nullable=True)
+    image_path = db.Column(String, nullable=True)
 
     def save_image(self, image):
         now = datetime.now()
