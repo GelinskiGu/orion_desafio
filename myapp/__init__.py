d43907bde51a7895410fa2aacd7f2dcaf986b428
Gustavo Gelinski <gus_gelinski@hotmail.com>
2023-05-26 20:06:29 -0300

    Ajustando deploy

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -7,6 +7,8 @@ from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_cl
 from datetime import datetime  # noqa: F401, E501
 import os
 from sqlalchemy.exc import SQLAlchemyError, InvalidRequestError, OperationalError  # noqa: F401, E501
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
 from .config import SQLALCHEMY_DATABASE_URI, SECRET_KEY  # noqa: F401, E501
 from .extensions import db, bcrypt
 from .models import User, Category, Recipe
